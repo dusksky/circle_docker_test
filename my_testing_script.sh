7acef5c192904e57e44a996848feb5ee3cb65d85
echigo-kazuya <dusksky.g@gmail.com>
2017-04-14 16:31:57 +0900

    phpunitのpath変更

--- my_testing_script.sh
+++ my_testing_script.sh
@@ -1,2 +1,2 @@
 mkdir -p $CIRCLE_TEST_REPORTS/phpunit
-phpunit --log-junit $CIRCLE_TEST_REPORTS/phpunit/junit.xml unittest.php
+vendor/bin/phpunit --log-junit $CIRCLE_TEST_REPORTS/phpunit/junit.xml unittest.php
